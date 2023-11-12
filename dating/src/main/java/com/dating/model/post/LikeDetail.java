efde64a7211ac9e17f64b34a1bac8c4a06ceb120
bondatfpt <bondatfpt@gmail.com>
2023-11-12 09:35:05 +0700

    ok

--- dating/src/main/java/com/dating/model/post/LikeDetail.java
+++ dating/src/main/java/com/dating/model/post/LikeDetail.java
@@ -1,4 +1,47 @@
 package com.dating.model.post;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class LikeDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime date;
+    @Column(columnDefinition = "boolean default false")
+    boolean isDeleted ;
+
+    public LikeDetail() {
+    }
+
+    public LikeDetail(Integer id, LocalDateTime date, boolean isDeleted) {
+        this.id = id;
+        this.date = date;
+        this.isDeleted = isDeleted;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
 }
--- dating/src/main/java/com/dating/model/post/Post.java
+++ dating/src/main/java/com/dating/model/post/Post.java
@@ -14,4 +14,54 @@ public class Post {
     @Column(columnDefinition = "boolean default false")
     boolean isDeleted;
 
+    public Post() {
+    }
+
+    public Post(Integer id, LocalDateTime date, String content, String image, boolean isDeleted) {
+        this.id = id;
+        this.date = date;
+        this.content = content;
+        this.image = image;
+        this.isDeleted = isDeleted;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
 }
