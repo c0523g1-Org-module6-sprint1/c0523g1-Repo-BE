ce12952d4eb63a1b78593784aec0d44003465106
hungcg <venenozelo94@gmail>
2023-11-12 16:47:50 +0700

    up

--- dating/src/main/java/com/dating/model/RelationshipStatus.java
+++ dating/src/main/java/com/dating/model/RelationshipStatus.java
@@ -1,2 +1,48 @@
-package com.dating.model;public class RelationshipStatus {
+package com.dating.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RelationshipStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private boolean isDeleted;
+
+    public RelationshipStatus() {
+    }
+
+    public RelationshipStatus(int id, String name, boolean isDeleted) {
+        this.id = id;
+        this.name = name;
+        this.isDeleted = isDeleted;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
 }
