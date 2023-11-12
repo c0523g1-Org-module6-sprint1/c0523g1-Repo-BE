32dfc38acc4053e5427eb0b241449d52351a81ac
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-11-12 18:56:22 +0700

    [ThienBB][Account security]

---
+++ dating/src/main/java/com/dating/controller/security/SecurityController.java
@@ -0,0 +1,52 @@
+package com.dating.controller.security;
+
+
+import com.dating.jwt.JwtUtility;
+import com.dating.payload.request.LoginRequest;
+import com.dating.payload.response.JwtResponse;
+import com.dating.service.impl.AccountDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/public")
+public class SecurityController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtility jwtUtility;
+
+    @PostMapping()
+    public ResponseEntity<?> authenticationUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtility.JwtTokenGenerator(loginRequest.getUsername());
+        AccountDetail accountDetail = (AccountDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String role = accountDetail.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()).toString();
+
+        return ResponseEntity.ok(
+                new JwtResponse(
+                        jwt,
+                        accountDetail.getId(),
+                        accountDetail.getUsername(),
+                        role
+                )
+        );
+    }
+}
