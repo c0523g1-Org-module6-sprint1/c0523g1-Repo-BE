747af3ff2d45eaba350a7c091ec3c5df198f55c9
bondatfpt <bondatfpt@gmail.com>
2023-11-13 15:31:30 +0700

    DatNC-CodingSpring

---
+++ dating/src/main/java/com/dating/service/post/IPrivacyPostService.java
@@ -0,0 +1,9 @@
+package com.dating.service.post;
+
+import com.dating.model.post.PrivacyPost;
+
+import java.util.List;
+
+public interface IPrivacyPostService {
+    List<PrivacyPost> showListPrivacyPost ();
+}
