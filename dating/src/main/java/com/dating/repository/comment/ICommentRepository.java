fa7b7a0f384e3a76760c7cd59beec68745f4520e
Binvie <thien97.night1@gmail.com>
2023-11-13 15:07:24 +0700

    [thienlch-Entity]

--- dating/src/main/java/com/dating/repository/comment/ICommentRepository.java
+++ dating/src/main/java/com/dating/repository/comment/ICommentRepository.java
@@ -1,9 +1,10 @@
 package com.dating.repository.comment;
 
+import com.dating.model.comment.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.xml.stream.events.Comment;
 
-public interface ICommentRepository extends JpaRepository<Comment,Integer> {
+public interface ICommentRepository extends JpaRepository<Comments,Integer> {
 
 }
