3522f109856216f9823a8bd659c4bc0ce4df96d1
Lien2009 <mylien200996@gmail.com>
2023-11-13 16:50:13 +0700

    [LienDTM][fix bug]

--- dating/src/main/java/com/dating/service/search_advanced_top_100/IAccountsService.java
+++ dating/src/main/java/com/dating/service/search_advanced_top_100/IAccountsService.java
@@ -5,7 +5,7 @@ import com.dating.model.account.Account;
 import java.util.List;
 
 public interface IAccountsService {
-    List<Account> getAll(String name, String birthday, int genderId, int hobbyId, int locationId, int jobId, int hobbyDetailId);
-
-    List<Account> findAll();
+//    List<Account> getAll(String name, String birthday, int genderId, int hobbyId, int locationId, int jobId, int hobbyDetailId);
+//
+//    List<Account> findAll();
 }
