ab0d19dc36c57dedf03527635c08cfcab3b41cd6
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-11-13 09:22:40 +0700

    [HanhNTM][create entity]

--- dating/src/main/java/com/dating/service/hobby_detail/IHobbyDetailService.java
+++ dating/src/main/java/com/dating/service/hobby_detail/IHobbyDetailService.java
@@ -1,4 +1,9 @@
 package com.dating.service.hobby_detail;
 
+import com.dating.model.hobby_detail.HobbyDetail;
+
+import java.util.List;
+
 public interface IHobbyDetailService {
+    List<HobbyDetail>  getAll();
 }
