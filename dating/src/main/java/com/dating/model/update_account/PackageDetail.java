99a1bb5b160928fad83e4f586260fcdc9da41a8a
hau146 <ishimaru274@gmail.com>
2023-11-13 09:05:25 +0700

    [HauNH][update account]

--- dating/src/main/java/com/dating/model/update_account/PackageDetail.java
+++ dating/src/main/java/com/dating/model/update_account/PackageDetail.java
@@ -1,16 +1,54 @@
 package com.dating.model.update_account;
 
+import com.dating.model.account.Account;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class PackageDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
-    private String abc;
+    @ManyToOne
+    @JoinColumn(name = "account_id",referencedColumnName = "id")
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "account_types_id",referencedColumnName = "id")
+    private AccountTypes accountTypes;
 
     public PackageDetail() {
     }
 
+    public PackageDetail(int id, Account account, AccountTypes accountTypes) {
+        this.id = id;
+        this.account = account;
+        this.accountTypes = accountTypes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public AccountTypes getAccountTypes() {
+        return accountTypes;
+    }
+
+    public void setAccountTypes(AccountTypes accountTypes) {
+        this.accountTypes = accountTypes;
+    }
 }
