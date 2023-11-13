afaa2d10ea5ac4e618dbf183e3ef0549661b33c1
hungcg <venenozelo94@gmail>
2023-11-13 09:52:27 +0700

    []

--- dating/src/main/java/com/dating/model/relationship/Relationships.java
+++ dating/src/main/java/com/dating/model/relationship/Relationships.java
@@ -1,11 +1,16 @@
 package com.dating.model.relationship;
+
 import com.dating.model.account.Account;
+import com.dating.model.gift.GiftRecord;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 
 @Entity
@@ -32,5 +37,9 @@ public class Relationships {
     @ManyToOne
     @JoinColumn(name = "account_id", referencedColumnName = "id")
     private Account receiverAccount;
+    @JsonBackReference
+    @OneToMany(mappedBy = "relationships")
+    private Set<RelationshipStatus> relationshipStatusSet;
+
 
 }
