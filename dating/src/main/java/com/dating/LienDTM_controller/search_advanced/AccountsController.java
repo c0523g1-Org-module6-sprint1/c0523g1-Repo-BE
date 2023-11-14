7f425093f5dba305b365ba44b1939259b7a2d071
Lien2009 <mylien200996@gmail.com>
2023-11-14 16:00:37 +0700

    [LienDTM][JUnit5]

--- dating/src/main/java/com/dating/LienDTM_controller/search_advanced/AccountsController.java
+++ dating/src/main/java/com/dating/LienDTM_controller/search_advanced/AccountsController.java
@@ -16,16 +16,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/search_advanced")
 public class AccountsController {
-    @Autowired
-    private IAccountsService accountsService;
-
-    @GetMapping("")
-    public ResponseEntity<List<Account>> getAll(String name, String birthday, int genderId, int hobbyId, int locationId, int jobId, int hobbyDetailId) {
-        List<Account> accounts = accountsService.getAll(name, birthday, genderId, hobbyId, locationId, jobId, hobbyDetailId);
-        if (accounts.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(accounts, HttpStatus.OK);
-        }
-    }
+//    @Autowired
+//    private IAccountsService accountsService;
+//
+//    @GetMapping("")
+//    public ResponseEntity<List<Account>> getAll(String name, String birthday, int genderId, int hobbyId, int locationId, int jobId, int hobbyDetailId) {
+//        List<Account> accounts = accountsService.getAll(name, birthday, genderId, hobbyId, locationId, jobId, hobbyDetailId);
+//        if (accounts.isEmpty()) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        } else {
+//            return new ResponseEntity<>(accounts, HttpStatus.OK);
+//        }
+//    }
 }
--- dating/src/main/java/com/dating/LienDTM_controller/top_100/AccountTopController.java
+++ dating/src/main/java/com/dating/LienDTM_controller/top_100/AccountTopController.java
@@ -16,16 +16,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/top_hundered")
 public class AccountTopController {
-    @Autowired
-    private IAccountsService accountsService;
-
-    @GetMapping("")
-    public ResponseEntity<List<Account>> getAll() {
-        List<Account> accounts = accountsService.findAll();
-        if (accounts.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(accounts, HttpStatus.OK);
-        }
-    }
+//    @Autowired
+//    private IAccountsService accountsService;
+//
+//    @GetMapping("")
+//    public ResponseEntity<List<Account>> getAll() {
+//        List<Account> accounts = accountsService.findAll();
+//        if (accounts.isEmpty()) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        } else {
+//            return new ResponseEntity<>(accounts, HttpStatus.OK);
+//        }
+//    }
 }
