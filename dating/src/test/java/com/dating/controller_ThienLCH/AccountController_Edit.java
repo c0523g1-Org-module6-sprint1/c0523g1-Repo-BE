2fa881c2bd0673fba054e18fe7e1fd89d0be8508
Binvie <thien97.night1@gmail.com>
2023-11-15 10:04:18 +0700

    okay

--- dating/src/test/java/com/dating/controller_ThienLCH/AccountController_Edit.java
+++ dating/src/test/java/com/dating/controller_ThienLCH/AccountController_Edit.java
@@ -2,7 +2,10 @@ package com.dating.controller_ThienLCH;
 
 import com.dating.dto.account.AccountDto;
 import com.dating.model.Role;
+import com.dating.model.gender.Gender;
+import com.dating.model.job.Job;
 import com.dating.model.location.Location;
+import com.dating.model.message.MessageStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +28,19 @@ public class AccountController_Edit {
     @Autowired
     private ObjectMapper objectMapper;
 
+    /**
+     * author: thienlch
+     * date: 14/11/2023
+     * goal: edit account with id equal ""
+     *
+     * @return HttpStatus 4xx
+     */
     @Test
-    public void editAccount_name_2() throws Exception {
-        AccountDto accountDto = new AccountDto();
-        accountDto.setName("");
+    public void editAccount_name_1() throws Exception {
+        AccountDto accountDto = new AccountDto(null, "Cristiano Ronaldo", "ronaldo123", "123456", "2023-10-10", "ronaldo@gmail.com",
+                "0932533481", 1000000.0, "2023-10-10", "https://wallpaperaccess.com/full/3090338.jpg", "2023-11-30", "Đã kết hôn", 100,
+                1, 1, 2, 1, 1);
+        accountDto.setName("ThienLCH");
         accountDto.setBirthday("2000-10-07");
         accountDto.setAvatar("link firebase");
         accountDto.setLocationId(3);
@@ -36,27 +48,60 @@ public class AccountController_Edit {
 
         this.mockMvc
                 .perform(MockMvcRequestBuilders
-                        .patch("/api/admin/edit/{id}"))
+                        .patch("/api/personal-page/edit/{id}", accountDto.getId()))
                 .andDo(print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is4xxClientError());
     }
 
+    /**
+     * author: thienlch
+     * date: 14/11/2023
+     * goal: edit account with id does not exist
+     *
+     * @return HttpStatus 4xx
+     */
     @Test
-    public void editAccount_name_4() throws Exception {
-
-        AccountDto accountDto = new AccountDto();
-        accountDto.setName("Hoan Thien");
-        accountDto.setBirthday("1997-10-07");
-        accountDto.setAvatar("link firebase-01");
-        accountDto.setLocationId(2);
-        accountDto.setJobId(2);
+    public void editAccount_name_2() throws Exception {
+        AccountDto accountDto = new AccountDto(7, "Cristiano Ronaldo", "ronaldo123", "123456", "2023-10-10", "ronaldo@gmail.com",
+                "0932533481", 1000000.0, "2023-10-10", "https://wallpaperaccess.com/full/3090338.jpg", "2023-11-30", "Đã kết hôn", 100,
+                1, 1, 2, 1, 1);
+        accountDto.setName("ThienLCH");
+        accountDto.setBirthday("2000-10-07");
+        accountDto.setAvatar("link firebase");
+        accountDto.setLocationId(3);
+        accountDto.setJobId(3);
 
         this.mockMvc
                 .perform(MockMvcRequestBuilders
-                        .patch("/api/admin/edit/{id}")
+                        .patch("/api/personal-page/edit/{id}", accountDto.getId()))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    /**
+     * author: thienlch
+     * date: 14/11/2023
+     * goal: edit account with real id
+     *
+     * @return HttpStatus 2xx
+     */
+    @Test
+    public void editAccount_name_4() throws Exception {
+
+        AccountDto accountDto = new AccountDto(1, "Cristiano Ronaldo", "ronaldo123", "123456", "2023-10-10", "ronaldo@gmail.com",
+                "0932533481", 1000000.0, "2023-10-10", "https://wallpaperaccess.com/full/3090338.jpg", "2023-11-30", "Đã kết hôn", 100,
+                1, 1, 2, 1, 1);
+        accountDto.setName("Thien Smoke");
+        accountDto.setAvatar("link firebase");
+        accountDto.setBirthday("1997-10-10");
+        accountDto.setLocationId(2);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .patch("/api/personal-page/edit/{id}", accountDto.getId())
                         .content(this.objectMapper.writeValueAsString(accountDto))
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andDo(print())
-                .andExpect(status().is4xxClientError());
+                .andExpect(status().is2xxSuccessful());
     }
 }
