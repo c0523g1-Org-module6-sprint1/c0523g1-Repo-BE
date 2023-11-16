f73e0d17d90d6214a563f01bbcb4265c1176c1c2
dinhlong1999 <long.truong.vcr@gmail.com>
2023-11-16 16:29:54 +0700

    [longtnd][personalpage]

--- dating/src/main/java/com/dating/config/WebSecurityConfig.java
+++ dating/src/main/java/com/dating/config/WebSecurityConfig.java
@@ -72,18 +72,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/public/**")
-                .permitAll()
-                .antMatchers("/api/member/**").hasAnyRole("MEMBER", "ADMIN")
-                .antMatchers("/api/admin/**").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .antMatchers("/api/public/**").permitAll()
+                .antMatchers("/api/member/**").hasAnyAuthority("MEMBER", "ADMIN")
+                .antMatchers("/api/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated().and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
