2fe6f39918f45e0e6cad2cbb9c3af92f8ef27a6f
dinhlong1999 <long.truong.vcr@gmail.com>
2023-11-16 15:50:55 +0700

    [longtnd][personalpage]

---
+++ dating/src/main/java/com/dating/dto/relationships/IRelationshipsDTO.java
@@ -0,0 +1,10 @@
+package com.dating.dto.relationships;
+
+public interface IRelationshipsDTO {
+    int getId();
+    int getIsDeleted();
+    int getRelationshipStatus();
+    int getSenderAccount();
+    int getReceiverAccount();
+
+}
