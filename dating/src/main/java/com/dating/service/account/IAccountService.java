e675df8518516dd880d43b7f7e42f6dfd8e9ef20
Binvie <thien97.night1@gmail.com>
2023-11-16 23:46:16 +0700

    [ThienLCH][updateAccount]

--- dating/src/main/java/com/dating/service/account/IAccountService.java
+++ dating/src/main/java/com/dating/service/account/IAccountService.java
@@ -1,6 +1,7 @@
 package com.dating.service.account;
 
 import com.dating.dto.account.AccountDTOs;
+import com.dating.dto.account.AccountDto;
 import com.dating.model.account.Account;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,4 +24,5 @@ public interface IAccountService extends UserDetailsService {
 
     //LongTND triển khai
     Account findAccountByUserName(String name);
+    Account setEditAccount(AccountDto accountDto);
 }
