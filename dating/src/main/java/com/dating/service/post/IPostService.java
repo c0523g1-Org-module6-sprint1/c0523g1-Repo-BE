0dc95570605d7a900fcf075e6b123de65e365da1
bondatfpt <bondatfpt@gmail.com>
2023-11-21 00:01:29 +0700

    DatNC

--- dating/src/main/java/com/dating/service/post/IPostService.java
+++ dating/src/main/java/com/dating/service/post/IPostService.java
@@ -10,11 +10,15 @@ public interface IPostService {
     boolean create( String content, String image, Integer accountId, Integer privacyId);
     List<Post> showListNewsfeed( Integer loggedInAccountId);
     List<Post> showListOfAnAccount( Integer accountId);
+    List<Post> showListForAdmin();
+    Boolean checkIsFriend (Integer accountId1, Integer accountId2);
     Post getPostById (Integer id);
     boolean updateForThePostOwner(String content, String image, Integer accountId, Integer postId, Integer privacyPostId);
     boolean updateForAdmin(String content, String image, Integer postId, Integer privacyPostId);
     boolean deleteForAdmin(Integer id);
     boolean deleteForThePostOwner(Integer id,Integer accountId);
+    List <Post> getListForFriend (@Param("accountId") Integer accountId);
+    List <Post> getListForStranger (@Param("accountId") Integer accountId);
 
 
 }
