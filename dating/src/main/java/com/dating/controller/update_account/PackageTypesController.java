a2ad790c90b4a091aba4a2226526295e0a6a9c94
hau146 <ishimaru274@gmail.com>
2023-11-21 11:35:28 +0700

    [HauNH][update account]

--- dating/src/main/java/com/dating/controller/update_account/PackageTypesController.java
+++ dating/src/main/java/com/dating/controller/update_account/PackageTypesController.java
@@ -1,14 +1,12 @@
 package com.dating.controller.update_account;
 
+import com.dating.dto.update_account.IPackageDto;
 import com.dating.model.update_account.PackageTypes;
 import com.dating.service.update_account.IPackageTypesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +25,15 @@ public class PackageTypesController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }else {
             return new ResponseEntity<>(packageTypesList,HttpStatus.OK);
+        }
+    }
+    @GetMapping("/packageAccount/{idAccount}")
+    public ResponseEntity<List<IPackageDto>> findAllPackageAccount(@PathVariable(value = "idAccount", required = false) int idAccount){
+        List<IPackageDto> packageDtos = packageTypesService.findAllPackageAccount(idAccount);
+        if(packageDtos.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(packageDtos,HttpStatus.OK);
         }
     }
 }
