16954d301f11e6ec5d9935aa5afbf8f5dabe8eb1
hungcg <venenozelo94@gmail>
2023-11-22 09:11:36 +0700

    HungHLP

--- dating/src/main/java/com/dating/service/account/IAccountService.java
+++ dating/src/main/java/com/dating/service/account/IAccountService.java
@@ -36,7 +36,6 @@ public interface IAccountService extends UserDetailsService {
      */
     Boolean createNewAccount(Account account);
 
-//    Page<Account> findAll(Pageable pageable,String username);
   
     void deleteAccount(Integer id);
 
